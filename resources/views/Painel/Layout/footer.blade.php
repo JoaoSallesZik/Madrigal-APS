c3dc763646460f09e2f595229d5f8a188ba82aea
Carlos <carlosvini1324@gmail.com>
2019-11-22 03:51:25 -0400

    Att 22/11/3:5027 v 2.1.2

--- resources/views/Painel/Layout/footer.blade.php
+++ resources/views/Painel/Layout/footer.blade.php
@@ -3,6 +3,6 @@
     Todos os direitos reservados.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 2.0
-      <small>Att 22/11/2019 02:30</small>
+      <small>Att 22/11/2019 03:50</small>
     </div>
   </footer>
